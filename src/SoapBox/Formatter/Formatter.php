b50441cc81dc0d86065997779456803d497d6b96
Jaspaul Bola <jaspaul.b@gmail.com>
2014-10-19 12:28:21 -0400

    Add constants for formats

---
+++ src/SoapBox/Formatter/Formatter.php
@@ -0,0 +1,11 @@
+<?php namespace SoapBox\Formatter;
+
+class Formatter {
+	/**
+	 * Add class constants that help define input format
+	 */
+	const Csv  = 'csv';
+	const Json = 'json';
+	const Xml  = 'xml';
+	const Arr  = 'array';
+}
---
+++ tests/unit/FormatterTest.php
@@ -0,0 +1,35 @@
+<?php namespace SoapBox\Formatter\Test;
+
+use SoapBox\Formatter\Formatter;
+
+class FormatterTest extends TestCase {
+
+	public function testFormatterProvidesCsvConstant() {
+		$expected = 'csv';
+		$actual = Formatter::Csv;
+
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function testFormatterProvidesJsonConstant() {
+		$expected = 'json';
+		$actual = Formatter::Json;
+
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function testFormatterProvidesXmlConstant() {
+		$expected = 'xml';
+		$actual = Formatter::Xml;
+
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function testFormatterProvidesArrayConstant() {
+		$expected = 'array';
+		$actual = Formatter::Arr;
+
+		$this->assertEquals($expected, $actual);
+	}
+
+}
