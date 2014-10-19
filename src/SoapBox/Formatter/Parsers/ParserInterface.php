d49335873eb4dfc35defea818c4935db81efae0d
Jaspaul Bola <jaspaul.b@gmail.com>
2014-10-19 16:56:21 -0400

    Add support for toXml from v1.4

--- src/SoapBox/Formatter/Parsers/ParserInterface.php
+++ src/SoapBox/Formatter/Parsers/ParserInterface.php
@@ -1,6 +1,7 @@
 <?php namespace SoapBox\Formatter\Parsers;
 
 use Spyc;
+use Str;
 
 /**
  * Parser Interface
@@ -41,4 +42,78 @@ abstract class ParserInterface {
 	public function toYaml() {
 		return Spyc::YAMLDump($this->toArray());
 	}
+
+	/**
+	 * To XML conversion
+	 *
+	 * @param   mixed        $data
+	 * @param   null         $structure
+	 * @param   null|string  $basenode
+	 * @return  string
+	 */
+	private function xmlify($data, $structure = null, $basenode = 'xml') {
+		// turn off compatibility mode as simple xml throws a wobbly if you don't.
+		if (ini_get('zend.ze1_compatibility_mode') == 1) {
+			ini_set('zend.ze1_compatibility_mode', 0);
+		}
+
+		if ($structure == null) {
+			$structure = simplexml_load_string("<?xml version='1.0' encoding='utf-8'?><$basenode />");
+		}
+
+		// Force it to be something useful
+		if (!is_array($data) && !is_object($data)) {
+			$data = (array) $data;
+		}
+
+		foreach ($data as $key => $value) {
+			// convert our booleans to 0/1 integer values so they are
+			// not converted to blanks.
+			if (is_bool($value)) {
+				$value = (int) $value;
+			}
+
+			// no numeric keys in our xml please!
+			if (is_numeric($key)) {
+				// make string key...
+				$key = (Str::singular($basenode) != $basenode) ? Str::singular($basenode) : 'item';
+			}
+
+			// replace anything not alpha numeric
+			$key = preg_replace('/[^a-z_\-0-9]/i', '', $key);
+
+			// if there is another array found recrusively call this function
+			if (is_array($value) or is_object($value)) {
+				$node = $structure->addChild($key);
+
+				// recursive call if value is not empty
+				if (!empty($value)) {
+					$this->xmlify($value, $node, $key);
+				}
+			} else {
+				// add single node.
+				$value = htmlspecialchars(html_entity_decode($value, ENT_QUOTES, 'UTF-8'), ENT_QUOTES, "UTF-8");
+
+				$structure->addChild($key, $value);
+			}
+		}
+
+		// pass back as string. or simple xml object if you want!
+		return $structure->asXML();
+	}
+
+	/**
+	 * Return an xml representation of the data stored in the parser
+	 *
+	 * @return string An xml string representing the encapsulated data
+	 */
+	public function toXml() {
+		return $this->xmlify($this->toArray());
+	}
+
+	/**
+	 * Return a csv representation of the data stored in the parser
+	 *
+	 * @return string An xml string representing the encapsulated data
+	 */
 }
